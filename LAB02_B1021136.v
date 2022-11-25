6cce0aaa9164918b385cab99bbe39b83015cf310
Sam-Wu-911117 <h4572306@gmail.com>
2022-11-25 21:10:29 +0800

    rewrite testbench

--- LAB02_B1021136.v
+++ LAB02_B1021136.v
@@ -1,82 +1,85 @@
-module HalfAdder (carry,sum,a,b);
-
-    input a,b;
-    output sum,carry;
-
-    xor #6 M1(sum,a,b);
-    and #5 M2(carry,a,b);
-    
+module Halfadder (c_out,sum,a,b);
+    input a, b;
+    output sum, c_out;
+        
+    xor #6 H0(sum,a,b);
+    and #5 H1(c_out,a,b);
 endmodule
 
-module FullAdder(sum,c_out,a,b,c_in);
+module Fulladder (sum,c_out,a,b,c_in);
     input a,b,c_in;
     output sum,c_out;
     wire w1,w2,w3;
-
-    HalfAdder M1(w2,w1,a,b);
-    HalfAdder M2(w3,sum,c_in,w1);
-    or #5 M3(c_out,w2,w3);
+    
+     Halfadder F0(w2,w1,a,b);
+     Halfadder F1(w3,sum,c_in,w1);
+     or #5 F2(c_out,w2,w3);
 endmodule
 
-module a_s_16_bits(oflow,c_out, sum, a, b, m);
-
-        input [15:0] a,b ; 
-        input m;
+module a_m_16_bits_delay_0 (s,c15,v,a,b,m);
+    input [15:0]a,b;
+    input m;
+    output c15,v;
+    output [15:0]s;
+    wire [15:0]b_out;
+    wire [14:0]c;
     
-        output oflow,c_out;
-        output  [15:0] sum;
-        wire [15:1] c;
-        wire [15:0] x;
+    Fulladder ST0(s[0],c[0],a[0],b_out[0],m);
+    Fulladder ST1(s[1],c[1],a[1],b_out[1],c[0]);
+    Fulladder ST2(s[2],c[2],a[2],b_out[2],c[1]);
+    Fulladder ST3(s[3],c[3],a[3],b_out[3],c[2]);
+    Fulladder ST4(s[4],c[4],a[4],b_out[4],c[3]);
+    Fulladder ST5(s[5],c[5],a[5],b_out[5],c[4]);
+    Fulladder ST6(s[6],c[6],a[6],b_out[6],c[5]);
+    Fulladder ST7(s[7],c[7],a[7],b_out[7],c[6]);
+    Fulladder ST8(s[8],c[8],a[8],b_out[8],c[7]);
+    Fulladder ST9(s[9],c[9],a[9],b_out[9],c[8]);
+    Fulladder ST10(s[10],c[10],a[10],b_out[10],c[9]);
+    Fulladder ST11(s[11],c[11],a[11],b_out[11],c[10]);
+    Fulladder ST12(s[12],c[12],a[12],b_out[12],c[11]);
+    Fulladder ST13(s[13],c[13],a[13],b_out[13],c[12]);
+    Fulladder ST14(s[14],c[14],a[14],b_out[14],c[13]);
+    Fulladder ST15(s[15],c15,a[15],b_out[15],c[14]);
+    xor #6 ST16(b_out[0],m,b[0]);
+    xor #6 ST17(b_out[1],m,b[1]);
+    xor #6 ST18(b_out[2],m,b[2]);
+    xor #6 ST19(b_out[3],m,b[3]);
+    xor #6 ST20(b_out[4],m,b[4]);
+    xor #6 ST21(b_out[5],m,b[5]);
+    xor #6 ST22(b_out[6],m,b[6]);
+    xor #6 ST23(b_out[7],m,b[7]);
+    xor #6 ST24(b_out[8],m,b[8]);
+    xor #6 ST25(b_out[9],m,b[9]);
+    xor #6 ST26(b_out[10],m,b[10]);
+    xor #6 ST27(b_out[11],m,b[11]);
+    xor #6 ST28(b_out[12],m,b[12]);
+    xor #6 ST29(b_out[13],m,b[13]);
+    xor #6 ST30(b_out[14],m,b[14]);
+    xor #6 ST31(b_out[15],m,b[15]);
+    xor #6 ST32(v,c15,c[14]);
+endmodule
 
-        assign #6 x[15:0] = b[15:0] ^ m;
-        
-        FullAdder FA1(sum[0],c[1],a[0],x[0],m);
-        FullAdder FA2(sum[1],c[2],a[1],x[1],c[1]);
-        FullAdder FA3(sum[2],c[3],a[2],x[2],c[2]);
-        FullAdder FA4(sum[3],c[4],a[3],x[3],c[3]);
-        FullAdder FA5(sum[4],c[5],a[4],x[4],c[4]);
-        FullAdder FA6(sum[5],c[6],a[5],x[5],c[4]);
-        FullAdder FA7(sum[6],c[7],a[6],x[6],c[5]);
-        FullAdder FA8(sum[7],c[8],a[7],x[7],c[6]);
-        FullAdder FA9(sum[8],c[9],a[8],x[8],c[7]);
-        FullAdder FA10(sum[9],c[10],a[9],x[9],c[8]);
-        FullAdder FA11(sum[10],c[11],a[10],x[10],c[9]);
-        FullAdder FA12(sum[11],c[12],a[11],x[11],c[10]);
-        FullAdder FA13(sum[12],c[13],a[12],x[11],c[11]);
-        FullAdder FA14(sum[13],c[14],a[13],x[13],c[12]);
-        FullAdder FA15(sum[14],c[15],a[14],x[14],c[13]);
-        FullAdder FA16(sum[15],c_out,a[15],x[15],c[14]);
-
-        xor #6 of(oflow,c_out,c[15]);
-endmodule    
-
-module adder_tb();
-reg  [15:0] a,b,ans;
-reg m;
-
-wire  [15:0] s; 
-wire c_out,oflow;
-
-integer check;
-integer handle,i,j,k;
-
-`define ADD 0
-`define SUB 1
-`define period 10
-
-a_s_16_bits as1(.oflow(oflow),.c_out(c_out),.sum(s),.a(a),.b(b),.m(m));
-
-//initial clk = 0;
-//always #(`period/2) clk = ~clk;
-initial begin 
-   handle=$fopen("delay_time.txt");
-      m= `ADD;
+module ST_bits();
+    wire [15:0] s;
+    wire c15,v;
+    
+    reg m;
+    reg [15:0] a,b,ans;
+    integer check;
+    integer handle_1,i,j,k;
+    
+    a_m_16_bits_delay_0 STBITS1(s,c15,v,a,b,m);
+    
+    initial 
+    begin
+      handle_1=$fopen("delay_time.txt");
+      m=0;
       for(i=0;i<=99;i=i+1)
       begin
         for(j=0;j<=99;j=j+1)
         begin
           check=1'b0;
-          a=i; b=j;
+          a=i;b=j;
           if(!m)
           begin
             m=~m;
@@ -86,9 +89,9 @@ initial begin
           begin
             ans=a-b;
           end
-
+          //------------------------testbench_output---------------------------------------
           for(k=1;k<=500;k=k+1)
-          begin 
+          begin
             #1
             if(ans==s)
             begin
@@ -101,10 +104,10 @@ initial begin
               
             if(check==20)
             begin
-              $fdisplay(handle,"%d  %d",$time-19,s);
+              $fdisplay(handle_1,"%d  %d",$time-19,s);
             end
           end
-        end  
-    end      
-end        
-endmodule
+        end
+      end
+    end
+    endmodule
